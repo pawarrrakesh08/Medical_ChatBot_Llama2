b928b8c85e74b127fe306c684e46a233776b108f
Rakesh Pawar <pawarrrakesh08@gmail.com>
2024-01-13 22:06:39 +0530

    setup.py file updated

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Medical ChatBot',
+    version = '0.0.0',
+    author = 'Rakesh Pawar',
+    author_email = 'pawarrrakesh08@gmail.com',
+    packages = find_packages(),
+    install_requires = [] 
+)
